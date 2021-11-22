a25c55d32d58386bc6647bf3b16c80b55a536c24
DiCostDi <qwert30320002@gmail.com>
2021-11-22 22:12:18 +0300

    Второе занятие, первая задача

---
+++ src/asemiakin/lab2/taskA1.java
@@ -0,0 +1,19 @@
+package asemiakin.lab2;
+
+import java.util.Scanner;
+
+public class taskA1 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число:");
+        a = scanner.nextInt();
+        System.out.print("Введите второе число:");
+        b = scanner.nextInt();
+        System.out.println("Сумма = " + (a + b) + ' ' + (double) (a + b)  +
+                ", Разность = " + (a - b) + ' ' + (double) (a - b) +
+                ", Произведение = " + (a * b)  + ' ' + (double) (a * b) +
+                ", Частное = " + (a / b) + ' ' + (double) (a / b) +
+                ", Остаток от деления = " + (a % b) + ' ' + (double) (a % b));
+    }
+}
