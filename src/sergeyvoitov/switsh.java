50d3643d59116f6dea2daf3cbb1ec7d9ceb3ecdd
voitovsergey <manankovapolina@gmail.com>
2021-11-22 22:23:03 +0300

    add lab1
    Signed-off-by: voitovsergey <manankovapolina@gmail.com>

--- src/sergeyvoitov/switsh.java
+++ src/sergeyvoitov/switsh.java
@@ -11,8 +11,8 @@ public class switsh {
         System.out.println("Введите число 2 ");
         int b = scaner.nextInt();
         System.out.println("Вы ввели" + a + "" + b);
-        System.out.println((a + b) + "" + (a - b) + "" + (a * b) + "" + (a / b) + "" + (a % b) );
-        System.out.println(((double)(a + b)) + "" + ((double)(a - b)) + "" + ((double)(a * b)) + "" + ((double)(a / b)) + "" + ((double)(a % b)) );
+        System.out.println((a + b) + "" + (a - b) + "" + (a * b) + "" + (a / b) + "" + (a % b));
+        System.out.println(((double) (a + b)) + "" + ((double) (a - b)) + "" + ((double) (a * b)) + "" + ((double) (a / b)) + "" + ((double) (a % b)));
 
-
+    }
 }
