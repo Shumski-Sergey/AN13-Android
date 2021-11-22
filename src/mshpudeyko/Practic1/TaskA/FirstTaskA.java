8fff390885fe45d34349789709f8c379e599adb9
MaxShpud <shpudeyko.02@gmail.com>
2021-11-22 22:28:33 +0300

    add sout

---
+++ src/mshpudeyko/Practic1/TaskA/FirstTaskA.java
@@ -0,0 +1,39 @@
+package mshpudeyko.Practic1.TaskA;
+
+import java.util.Scanner;
+
+
+public class FirstTaskA {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a: " );
+        int a=scanner.nextInt();
+        System.out.print("Enter b: " );
+        int b=scanner.nextInt();
+        System.out.println("Hello world");
+        int res;
+        res=a+b;
+        System.out.print("Sum "+res+"\t");
+        res=a-b;
+        System.out.print("Dif "+res+"\t");
+        res=a*b;
+        System.out.print("Prod "+res+"\t");
+        res=a/b;
+        System.out.print("Chas "+res+"\t");
+        res=a%b;
+        System.out.println("Ost "+res+"\t");
+
+        double SecondRes=a+b;
+        System.out.print("Sum "+SecondRes+"\t");
+        SecondRes=a-b;
+        System.out.print("Dif "+SecondRes+"\t");
+        SecondRes=a*b;
+        System.out.print("Prod "+SecondRes+"\t");
+        SecondRes=a/b;
+        System.out.print("Chas "+SecondRes+"\t");
+        SecondRes=a%b;
+        System.out.print("Ost "+SecondRes+"\t");
+
+    }
+}
