bbe09f6982b1142dcc8e30f9beb6474eedd419a7
ImaWalkerr <anesteshia95@mail.ru>
2021-11-23 00:44:03 +0300

    практика 2е занятие.

---
+++ src/mkornilkov/lesson2/lesson2/task1.java
@@ -0,0 +1,24 @@
+package mkornilkov.lesson2.lesson2;
+
+import java.util.Scanner;
+
+public class task1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        {System.out.println("Введите целое число A");}
+        int a = sc.nextInt();
+        {System.out.println("Введите целое число B");}
+        int b = sc.nextInt();
+        {System.out.print ("a+b="+(a+b));}
+        {System.out.print (" a-b="+(a-b));}
+        {System.out.print (" a*b="+(a*b));}
+        {System.out.print (" a/b="+(a/b));}
+        {System.out.println (" a%b="+(a%b));}
+
+        {System.out.print ("a+b="+(double)(a+b));}
+        {System.out.print (" a-b="+(double)(a-b));}
+        {System.out.print (" a*b="+(double)(a*b));}
+        {System.out.print (" a/b="+(double)(a/b));}
+        {System.out.println (" a%b="+(double)(a%b));}
+    }
+}
