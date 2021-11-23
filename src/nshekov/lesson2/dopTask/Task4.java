c114420ff8b845360c660d95b5387be91f201d3d
NikitaShekov <shekovnikita8@gmail.com>
2021-11-23 18:42:26 +0300

    Lesson2 DopTask4

---
+++ src/nshekov/lesson2/dopTask/Task4.java
@@ -0,0 +1,19 @@
+package nshekov.lesson2.dopTask;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("Enter three of your numbers:");
+        Scanner sc = new Scanner(System.in);
+        int counter=0;
+        //int a = sc.nextInt();
+        //int b = sc.nextInt();
+        //int c = sc.nextInt();
+        for (int i = 0; i < 3; i++) {
+            if (sc.nextInt() > 0)
+                counter++;
+        }
+        System.out.println(counter);
+    }
+}
