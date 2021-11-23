4824dc7dd6002c179419f48c46a5b7af8cc3b7ea
Alekseenko-Aleksej <alekseenkoaleksej@gmail.com>
2021-11-23 15:39:25 +0300

    Example

---
+++ src/aalekseenko/Lection1/TaskA.java
@@ -0,0 +1,7 @@
+package aalekseenko.Lection1;
+
+public class TaskA {
+    public static void main(String[] args) {
+                System.out.println("Hello World!");
+    }
+}
---
+++ src/aalekseenko/Lection1/TaskA2.java
@@ -0,0 +1,11 @@
+package aalekseenko.Lection1;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        System.out.println("Я начинаю изучать Java");
+        System.out.println("Я начинаю изучать Java");
+        System.out.println("Я начинаю изучать Java");
+        System.out.println("Я начинаю изучать Java");
+        System.out.println("Я начинаю изучать Java");
+    }
+}
