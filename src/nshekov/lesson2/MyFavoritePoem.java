08851936e927acd1e00433ac377d66c8282eaa0a
NikitaShekov <shekovnikita8@gmail.com>
2021-11-23 10:31:50 +0300

    Lesson2 MyFavoritePoem

---
+++ src/nshekov/lesson2/MyFavoritePoem.java
@@ -0,0 +1,13 @@
+package nshekov.lesson2;
+
+public class MyFavoritePoem {
+    public static void main(String[] args) {
+        System.out.println("Моё любимое стихотворение:");
+        String a, b, c, d;
+        a = "Даже если Вам немного за тридцать,";
+        b = "Есть надежда выйти замуж за принца.";
+        c = "Солнце всем на планете одинаково светит";
+        d = "И принцессе, и простой проводнице.";
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
+    }
+}
