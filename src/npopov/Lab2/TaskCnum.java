ea7b52753332d80a99dfe7248dfd520a5457583d
NicolaiPopov168 <71674594+NicolaiPopov168@users.noreply.github.com>
2021-11-23 11:39:07 +0300

    Lab2: TaskC(sumDigitsInNumber)

---
+++ src/npopov/Lab2/TaskCnum.java
@@ -0,0 +1,17 @@
+package npopov.Lab2;
+
+import java.util.Scanner;
+
+public class TaskCnum {
+
+    public static int sumDigitsInNumber(int number) {
+        return (number%10)+(number%100-number%10)/10+(number%1000-number%100)/100+(number-number%1000)/1000;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int n = scanner.nextInt();
+        System.out.println(sumDigitsInNumber(n));
+    }
+}
