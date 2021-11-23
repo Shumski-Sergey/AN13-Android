d7a48e729fbea0b3933b444a53f54b95206bac32
VitkoKirill <vitkokirill0077@gmail.com>
2021-11-23 20:03:13 +0300

    Lab2 task_b2

---
+++ src/kvitko/Lab2/Lab2_task_A.java
@@ -0,0 +1,32 @@
+package kvitko.Lab2;
+
+import java.util.Scanner;
+
+public  class Lab2_task_A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter b: ");
+        int b = scanner.nextInt();
+        System.out.println(a+b+" "+(a-b)+" "+a*b+" "+a/b+" "+a%b);
+        System.out.println((double)a+b+" "+(double)(a-b)+" "+(double)a*b+" "+(double)a/b+" "+(double)a%b);
+
+        System.out.println("--------------------");
+
+        String A="Я помню чудное мгновенье:";
+        String B="Передо мной явилась ты,";
+        String C="Как мимолетное виденье,";
+        String D="Как гений чистой красоты.";
+        String E="В томленьях грусти безнадежной,";
+        String F="В тревогах шумной суеты,";
+        String G="Звучал мне долго голос нежный";
+        String H="И снились милые черты.";
+
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println(A+"\n"+B+"\n"+C+"\n"+D+"\n"+E+"\n"+F+"\n"+G+"\n"+H);
+    }
+}
+
+
+
---
+++ src/kvitko/Lab2/Lab2_task_B.java
@@ -0,0 +1,38 @@
+package kvitko.Lab2;
+
+import java.util.Scanner;
+
+public class Lab2_task_B {
+
+
+    public static double dis(int a, int b, int c) {
+        return b^2+4*a*c;
+    }
+
+    public static void main(String[] args) {
+
+        double d, x1,x2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a*x^2+b*x+c=0");
+        System.out.print("Введите число a:");
+        int a = scanner.nextInt();
+        System.out.print("Введите число b:");
+        int b = scanner.nextInt();
+        System.out.print("Введите число c:");
+        int c = scanner.nextInt();
+        System.out.println("a*x^2+b*x+c=0");
+        d=dis(a,b,c);
+        d=Math.sqrt(d);
+        if (d>0){
+            x1=(-b-d)/2*a;
+            x2=(-b+d)/2*a;
+            System.out.println(x1+" "+x2);
+        } else if (d==0){
+                x1=-b/2*a;
+                System.out.println(x1);
+            }
+        else if(d<0){
+            System.out.println("Отрицательный дискриминант");
+        }
+    }
+}
