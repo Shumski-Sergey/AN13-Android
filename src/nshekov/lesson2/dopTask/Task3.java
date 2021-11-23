440d75bf4559f25ac2ec707d9abcf6843eb7435f
NikitaShekov <shekovnikita8@gmail.com>
2021-11-23 18:28:46 +0300

    Lesson2 DopTask3

---
+++ src/nshekov/lesson2/dopTask/Task3.java
@@ -0,0 +1,19 @@
+package nshekov.lesson2.dopTask;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Enter your number:");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0){
+            number += 1;
+        } else if (number < 0){
+            number -=2;
+        } else {
+            number = 10;
+        }
+        System.out.println(number);
+    }
+}
