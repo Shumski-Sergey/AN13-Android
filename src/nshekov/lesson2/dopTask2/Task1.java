f70f187061a6ac0436d0455f8221469995a4b2f3
NikitaShekov <shekovnikita8@gmail.com>
2021-11-25 11:10:49 +0300

    Lesson2 Dop2_Task1,2

---
+++ src/nshekov/lesson2/dopTask2/Task1.java
@@ -0,0 +1,17 @@
+package nshekov.lesson2.dopTask2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Enter your number:");
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+        char[] numchar = number.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++){
+            sum += Character.digit(numchar[i], 10);
+        }
+        System.out.println("Summa is:\n" + sum);
+    }
+}
---
+++ src/nshekov/lesson2/dopTask2/Task2.java
@@ -0,0 +1,23 @@
+package nshekov.lesson2.dopTask2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int j = 0;
+        for (int i = 1; i <= number; i++) {
+            int ost = number % i;
+            if (ost == 0){
+                j++;
+            }
+        }
+        if (j == 2){
+            System.out.println("Your number is simple");
+        } else {
+            System.out.println("Yoyr number is not simple");
+        }
+    }
+}
