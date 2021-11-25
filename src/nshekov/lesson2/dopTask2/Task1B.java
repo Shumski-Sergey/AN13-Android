27569b02bef3f6df23934c4a33912e02d1364d56
NikitaShekov <shekovnikita8@gmail.com>
2021-11-25 12:16:25 +0300

    Lesson2 Dop2_Task1B

---
+++ src/nshekov/lesson2/dopTask2/Task1B.java
@@ -0,0 +1,15 @@
+package nshekov.lesson2.dopTask2;
+
+import java.util.Scanner;
+
+public class Task1B {
+    public static void main(String[] args) {
+        System.out.println("Enter your number:");
+        Scanner sc = new Scanner(System.in);
+        int fuct = sc.nextInt();
+        for (int i = fuct - 1; i > 0 ; i--) {
+            fuct *= i;
+        }
+        System.out.println(fuct);
+    }
+}
