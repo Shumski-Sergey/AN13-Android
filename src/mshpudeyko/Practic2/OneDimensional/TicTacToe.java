7da7fa8188e70c69ce54d3b68d08990137f150a0
MaxShpud <shpudeyko.02@gmail.com>
2021-11-26 20:57:57 +0300

    Some tasks and the tic-tac-toe game

--- src/mshpudeyko/Practic2/OneDimensional/TicTacToe.java
+++ src/mshpudeyko/Practic2/OneDimensional/TicTacToe.java
@@ -3,7 +3,7 @@ package mshpudeyko.Practic2.OneDimensional;
 import java.util.Scanner;
 
 public class TicTacToe {
-    public static void print(char array[][]) {
+    public static void print(char[][] array) {
         System.out.println("Текущее состояние поля:");
         for (int i = 0, top = 0, side = 0; i < 3; i++, side++) {
             if (top == 0) {
@@ -19,7 +19,7 @@ public class TicTacToe {
         System.out.println();
     }
 
-    public static boolean checkRows(char array[][], char player1, char player2) {
+    public static boolean checkRows(char[][] array, char player1, char player2) {
         for (int i = 0, j = 0; i < 3; i++) {
             if (array[i][j] == player1 && array[i][j + 1] == player1 && array[i][j + 2] == player1) {
                 print(array);
@@ -35,7 +35,7 @@ public class TicTacToe {
         return false;
     }
 
-    public static boolean checkCols(char array[][], char player1, char player2) {
+    public static boolean checkCols(char[][] array, char player1, char player2) {
         for (int i = 0, j = 0; j < 3; j++) {
             if (array[i][j] == player1 && array[i + 1][j] == player1 && array[i + 2][j] == player1) {
                 print(array);
@@ -51,35 +51,33 @@ public class TicTacToe {
         return false;
     }
 
-    public static boolean checkDiagonals(char array[][], char player1, char player2) {
-        while (true) {
-            int i = 0, j = 0;
-            if (array[i][j] == player1 && array[i + 1][j + 1] == player1 && array[i + 2][j + 2] == player1) {
-                print(array);
-                System.out.println("Игрок 1 выиграл!");
-                return true;
-            }
-            if (array[i][j] == player2 && array[i + 1][j + 1] == player2 && array[i + 2][j + 2] == player2) {
-                print(array);
-                System.out.println("Игрок 2 выиграл!");
-                return true;
-            }
-            if (array[i][j + 2] == player1 && array[i + 1][j + 1] == player1 && array[i + 2][j] == player1) {
-                print(array);
-                System.out.println("Игрок 1 выиграл!");
-                return true;
-            }
-            if (array[i][j + 2] == player2 && array[i + 1][j + 1] == player2 && array[i + 2][j] == player2) {
-                print(array);
-                System.out.println("Игрок 2 выиграл!");
-                return true;
-            }
-            break;
+    public static boolean checkDiagonals(char[][] array, char player1, char player2) {
+        int i = 0, j = 0;
+        if (array[i][j] == player1 && array[i + 1][j + 1] == player1 && array[i + 2][j + 2] == player1) {
+            print(array);
+            System.out.println("Игрок 1 выиграл!");
+            return true;
         }
+        if (array[i][j] == player2 && array[i + 1][j + 1] == player2 && array[i + 2][j + 2] == player2) {
+            print(array);
+            System.out.println("Игрок 2 выиграл!");
+            return true;
+        }
+        if (array[i][j + 2] == player1 && array[i + 1][j + 1] == player1 && array[i + 2][j] == player1) {
+            print(array);
+            System.out.println("Игрок 1 выиграл!");
+            return true;
+        }
+        if (array[i][j + 2] == player2 && array[i + 1][j + 1] == player2 && array[i + 2][j] == player2) {
+            print(array);
+            System.out.println("Игрок 2 выиграл!");
+            return true;
+        }
+
         return false;
     }
 
-    public static boolean draw(char array[][], char player1, char player2) {
+    public static boolean draw(char[][] array, char player1, char player2) {
         for (int i = 0, k = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (array[i][j] == player1 || array[i][j] == player2) {
@@ -98,7 +96,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Размерность поля для игры в крестики-нолики будет (3х3)");
-        char array[][] = new char[3][3];
+        char[][] array = new char[3][3];
         System.out.println("В этой игре могут участвовать только два игрока\nИгрок 1-нолик(0), игрок 2-крестик(X)");
         char player1 = 'O', player2 = 'X';
         int cols, rows;
@@ -122,10 +120,10 @@ public class TicTacToe {
                     break;
                 }
             }
-            if (checkRows(array, player1, player2) == true) return;
-            if (checkCols(array, player1, player2) == true) return;
-            if (checkDiagonals(array, player1, player2) == true) return;
-            if (draw(array, player1, player2) == true) return;
+            if (checkRows(array, player1, player2)) return;
+            if (checkCols(array, player1, player2)) return;
+            if (checkDiagonals(array, player1, player2)) return;
+            if (draw(array, player1, player2)) return;
             while (true) {
                 System.out.print("Игрок 2, Ваш ход!\nВыберите строку и столбец: ");
                 cols = scanner.nextInt();
