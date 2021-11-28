af0402c5973daa4b61fb34abed6dbd03be10310a
Vania311 <vanek311@gmail.com>
2021-11-28 20:16:15 +0300

    Task4

---
+++ src/ibagritsevich/lection_2/Task4.java
@@ -0,0 +1,21 @@
+package ibagritsevich.lection_2;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input numbers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int positive_num = 0;
+        if (num1<=0 && num2<=0 && num3<=0)
+            System.out.println("Положительных чисел нет");
+        if (num1 > 0) positive_num++;
+        if (num2 > 0) positive_num++;
+        if (num3 > 0) positive_num++;
+        System.out.println("Количество положительных чисел: " + positive_num);
+    }
+}
+
