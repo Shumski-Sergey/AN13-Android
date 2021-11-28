36ea6983092f0c6b3cbf385d92f98bd071f179d8
Vania311 <vanek311@gmail.com>
2021-11-28 14:52:42 +0300

    TaskA2

---
+++ src/ibagritsevich/lection_1/TaskA2.java
@@ -0,0 +1,10 @@
+package ibagritsevich.lection_1;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            System.out.println("Я начинаю изучать Java");
+        }
+    }
+}
