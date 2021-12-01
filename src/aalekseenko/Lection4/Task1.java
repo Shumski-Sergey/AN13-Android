5ddcb198fc8698104fceb6eb6fd4a1428d3dcf8b
Alekseenko-Aleksej <alekseenkoaleksej@gmail.com>
2021-12-01 21:52:13 +0300

    Lection4 task1-2

---
+++ src/aalekseenko/Lection4/Task1.java
@@ -0,0 +1,26 @@
+package aalekseenko.Lection4;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество строк: ");
+        int CountStr = sc.nextInt();
+        System.out.println("Строка №1: ");
+        String MaxLine = sc.next();
+        String MinLine = MaxLine;
+        for (int i = 2; i <= CountStr; i++) {
+            System.out.println("Строка" + "№" + i + ": ");
+            String next = sc.next();
+            if (MaxLine.length() < next.length()) {
+                MaxLine = next;
+            } else if (MinLine.length() > next.length()) {
+                MinLine = next;
+            }
+        }
+        System.out.println("MaxLine = ".concat(MaxLine) + " Length = " + MaxLine.length());
+        System.out.print("MinLine = ".concat(MinLine) + "  Length = " + MinLine.length());
+    }
+    }
+
---
+++ src/aalekseenko/Lection4/Task2.java
@@ -0,0 +1,48 @@
+package aalekseenko.Lection4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Task2 {
+/*
+    Ввести n строк с консоли. Упорядочить и вывести строки в порядке возрастания (убывания) значений их длины.
+*/
+    static class StringLengthSort implements Comparator<String>{
+        @Override
+        public int compare(String o1, String o2) {
+            if(o1.length() > o2.length()){
+                return 1;
+            }else{
+                if(o1.length() < o2.length()){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество строк: ");
+        int CountStr = sc.nextInt();
+        String [] MassStr = new String[CountStr];
+        for (int i = 0; i < CountStr; i++) {
+            System.out.println("Введите строку №" + (i+1));
+            Scanner ssc = new Scanner(System.in);
+            MassStr[i] = ssc.next();
+        }
+            Comparator<String> stringLengthComparator = new StringLengthSort();
+            Arrays.sort(MassStr, stringLengthComparator);
+            System.out.println("\nОтсортировано по возрастанию длин строк:\n");
+            for(String str : MassStr){
+                System.out.println(str);
+            }
+        System.out.println("\nОтсортировано по убыванию длин строк:\n");
+        for(int i=(CountStr-1); i>=0; i--) {
+            System.out.println(MassStr[i]);
+        }
+
+    }
+}
