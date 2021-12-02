652ec6054514dd9c832288791d5e97922b5d9e40
Vania311 <vanek311@gmail.com>
2021-12-02 10:23:55 +0300

    Task

--- src/ibagritsevich/lection_2/Task1.java
+++ src/ibagritsevich/lection_2/Task1.java
@@ -1,45 +1,47 @@
-//package ibagritsevich.lection_2;
+package ibagritsevich.lection_2;
 
-//public class Task1 {
-   // public static void znak(int num){
-    //    if (num>0){
-     //       System.out.print("положительное");
-    //    }
-   //     if (num<0){
-    //        System.out.print("отрицательное");
-  //      }
-  //  }
+import java.util.Scanner;
 
-   // public static void main(String[] args) {
-     //   Scanner scanner = new Scanner(System.in);
-     //   System.out.println("input number: ");
-     //   int num = scanner.nextInt();
-     //   int i = 0;
-     //   int res = num;
+public class Task1 {
+    public static void znak(int num) {
+        if (num > 0) {
+            System.out.print("положительное");
+        }
+        if (num < 0) {
+            System.out.print("отрицательное");
+        }
+    }
 
-     //   if (num != 0) {
-    //        while (num != 0) {
-     //           num = num / 10;
-     //           i++;
-      //      }
-         //   switch (i) {
-       //         case 1 -> {
-      //              System.out.println("число однозначное");
-       //             znak(res);
-       //         }
-       //         case 2 -> {
-       //             System.out.println("число двухзначное");
-         //           znak(res);
-          //      }
-      //          case 3 -> {
-      //              System.out.println("число трехзначное");
-      //              znak(res);
-       //         }
-       //         default -> {
-        //            System.out.println("число четырехзначное и более");
-       //             znak(res);
-      //          }
-      //      }
-      //  }
-//    }
-//}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input number: ");
+        int num = scanner.nextInt();
+        int i = 0;
+        int res = num;
+
+        if (num != 0) {
+            while (num != 0) {
+                num = num / 10;
+                i++;
+            }
+            switch (i) {
+                case 1 -> {
+                    System.out.println("число однозначное");
+                    znak(res);
+                }
+                case 2 -> {
+                    System.out.println("число двухзначное");
+                    znak(res);
+                }
+                case 3 -> {
+                    System.out.println("число трехзначное");
+                    znak(res);
+                }
+                default -> {
+                    System.out.println("число четырехзначное и более");
+                    znak(res);
+                }
+            }
+        }
+    }
+}
