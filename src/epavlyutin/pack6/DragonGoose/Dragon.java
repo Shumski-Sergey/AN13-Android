25db709bf587b98d4c9275b34cc3a22f6a8ff80d
SeniorEgor <crossfirerules@mail.ru>
2021-12-08 18:24:20 +0300

    Lab6 Task2
    Signed-off-by: SeniorEgor <crossfirerules@mail.ru>

---
+++ src/epavlyutin/pack6/DragonGoose/Dragon.java
@@ -0,0 +1,8 @@
+package epavlyutin.pack6.DragonGoose;
+
+public class Dragon extends BigAnimal{
+    @Override
+    public String getSize() {
+        return super.getSize();
+    }
+}
---
+++ src/epavlyutin/pack6/DragonGoose/Goose.java
@@ -0,0 +1,8 @@
+package epavlyutin.pack6.DragonGoose;
+
+public class Goose extends SmallAnimal {
+    @Override
+    public String getSize() {
+        return super.getSize();
+    }
+}
