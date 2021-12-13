fadec12cf4f892ab2817ce5cbf6f8e34a4024fe0
neformatic <reznik.workspace@gmail.com>
2021-12-13 21:09:04 +0300

    AN13-8 Change Runner task for exception lab
    -Add OPP sample for interface lab
    -Change runner for lab exception
    -Public interface Employee.java for Some.java lab №7

--- src/sshumski/Lab5/Runner.java
+++ src/sshumski/Lab5/Runner.java
@@ -1,27 +1,19 @@
 package sshumski.Lab5;
 
-/**
- * Создать класс Person, который содержит:
- * a) поля fullName, age.
- * б) методы move() и talk(), в которых просто вывести на
- * консоль сообщение -"Такой-то  Person говорит".
- * в) Добавьте два конструктора  - Person() и Person(fullName, age).
- * Создайте два объекта этого класса.
- * Один объект инициализируется конструктором Person(),
- * другой - Person(fullName, age).
- */
+
+import com.sun.org.apache.xml.internal.security.exceptions.AlgorithmAlreadyRegisteredException;
 
 public class Runner {
-    //TODO @shumski.s: Что делает переменная?! Допиши плизз!!
-    private static final int OFFSET_IMAGE = 1043;
-
-    private Character name;
-    public static void main(String[] args) {
-        Person firstPerson = new Person();
-        Person secondPerson = new Person(18-1, "Robert");
-        System.out.println(secondPerson);
-        System.out.println(OFFSET_IMAGE);
-        firstPerson.talk();
-        secondPerson.talk();
+    public static void main(String[] args) throws Exception {
+        try {
+            some();
+        } catch (IllegalAccessException | AlgorithmAlreadyRegisteredException e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void some() throws Exception {
+        throw new Exception("Nsafa");
+    }
+
 }
