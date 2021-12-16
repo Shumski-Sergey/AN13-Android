227fdd882fe1781e2a7d27c45d7cce5a7fa33b3d
Sergey Shumski <sergey.shumski@neotech.by>
2021-12-17 01:50:59 +0300

    Add JSONObject converter to project by sample

--- src/sshumski/Lab8/JsonReader.java
+++ src/sshumski/Lab8/JsonReader.java
@@ -1,22 +1,28 @@
 package sshumski.Lab8;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class JsonReader {
-    public static String readJSONFaceBookGraph(String url) throws IOException {
+    public static JSONObject readJSONByUrl(String url) throws IOException {
         InputStream is = new URL(url).openStream();
+        JSONObject jsonObject = null;
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String jsonText = readAll(rd);
-//            JSONParser parser = new JSONParser(jsonText);
-//            JsonTestUrl json = (JsonTestUrl) parser.parse(jsonText);
-
-            return jsonText;
+            JSONParser parser = new JSONParser();
+            jsonObject = (JSONObject) parser.parse(jsonText);
+        } catch (ParseException e) {
+            e.printStackTrace();
         } finally {
             is.close();
         }
+        return jsonObject;
     }
 
     private static String readAll(Reader rd) throws IOException {
--- src/sshumski/Lab8/UrlGetJSONExample.java
+++ src/sshumski/Lab8/UrlGetJSONExample.java
@@ -1,23 +1,25 @@
 package sshumski.Lab8;
 
+import org.json.simple.JSONObject;
+
 import java.io.IOException;
 
-import static sshumski.Lab8.JsonReader.readJSONFaceBookGraph;
+import static sshumski.Lab8.JsonReader.readJSONByUrl;
 
 public class UrlGetJSONExample {
     private static final String GRAPH_FACEBOOK = "http://headers.jsontest.com/";
 
     public static void main(String[] args) {
-        String json = null;
+        JSONObject jsonObject = null;
         try {
-            json = readJSONFaceBookGraph(GRAPH_FACEBOOK);
+            jsonObject = readJSONByUrl(GRAPH_FACEBOOK);
         } catch (IOException e) {
             System.out.println("Ошибка при скачивании информации URL - "
                     + e.getMessage());
             e.printStackTrace();
         }
-        assert json != null;
-        System.out.println(json);
-//      System.out.println(json.get("id"));
+        assert jsonObject != null;
+        // Для нормального преобразования потребудется еще одна библиотека https://github.com/google/gson
+        System.out.println(jsonObject.get("Accept"));
     }
 }
