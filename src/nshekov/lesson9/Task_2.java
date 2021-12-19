5370219d2e0cea6ba668070fff5e6e4f69d489f7
NikitaShekov <shekovnikita8@gmail.com>
2021-12-19 10:24:02 +0300

    Lesson9 TaskA2 reload
    1. Added array "Files"
    2. Replaced block "foreach"

--- src/nshekov/lesson9/Task_2.java
+++ src/nshekov/lesson9/Task_2.java
@@ -9,12 +9,13 @@ import java.text.SimpleDateFormat;
 public class Task_2 {
     public static void main(String[] args) {
         File file = new File("D:\\Программы\\Koding\\AN13-Android\\src\\nshekov\\lesson9", "Task_2.txt");
-        File fileInfo = new File("D:\\Программы\\Koding\\AN13-Android\\src\\nshekov\\lesson9");   //проверяемая папка
+        File fileInfo = new File("D:\\Программы\\Koding\\AN13-Android\\src\\nshekov\\lesson3");   //проверяемая папка
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
         File [] files = fileInfo.listFiles();
         try {
             FileWriter writer = new FileWriter(file, false);
             BufferedWriter bufferWriter = new BufferedWriter(writer);
+            assert files != null;
             for (File a: files) {
                     bufferWriter.write(a.getPath() + "\t" + sdf.format(a.lastModified()) + "\n");
             }
