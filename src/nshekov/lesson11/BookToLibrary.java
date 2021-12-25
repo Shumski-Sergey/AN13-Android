a354df84e929216a8ca2d9b44381565490940391
NikitaShekov <shekovnikita8@gmail.com>
2021-12-26 01:29:36 +0300

    Lesson11 Library

---
+++ src/nshekov/lesson11/BookToLibrary.java
@@ -0,0 +1,33 @@
+package nshekov.lesson11;
+
+public class BookToLibrary extends Thread{
+    private final String bookName;
+    private boolean flag = true;
+
+    public BookToLibrary(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public synchronized void getBook(String personName){
+        while (!flag){
+            try {
+                System.out.println(bookName + " пока кто-то читает в библиотеке");
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(personName + " взял(а) " + bookName);
+        flag = false;
+    }
+    public synchronized void putBook(String personName){
+        System.out.println(personName + " вернул(а) " + bookName);
+        flag = true;
+        notify();
+    }
+
+}
---
+++ src/nshekov/lesson11/Person.java
@@ -0,0 +1,50 @@
+package nshekov.lesson11;
+
+public class Person extends Thread{
+    private final String personName;
+    private BookToHome bookToHome;
+    private BookToLibrary bookToLibrary;
+
+    public Person(String personName, BookToHome b) {
+        new Thread(b).start();
+        this.personName = personName;
+        this.bookToHome = b;
+    }
+
+    public Person(String personName, BookToHome b, BookToLibrary a) {
+        new Thread(b).start();
+        this.personName = personName;
+        this.bookToHome = b;
+        this.bookToLibrary = a;
+    }
+
+    public Person(String personName, BookToLibrary b) {
+        new Thread(b).start();
+        this.personName = personName;
+        this.bookToLibrary = b;
+    }
+
+    public void run() {
+        if (bookToHome != null) {
+            System.out.println(personName + " хочет взять книгу " + bookToHome.getBookName() + " домой");
+            bookToHome.getBook(personName);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            bookToHome.putBook(personName);
+        }
+        if (bookToLibrary != null){
+            System.out.println(personName + " хочет взять книгу " + bookToLibrary.getBookName() + " почитать в библиотеке");
+            bookToLibrary.getBook(personName);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            bookToLibrary.putBook(personName);
+        }
+    }
+
+}
